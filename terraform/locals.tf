1aa40a3cc2520eeec06f0d7167ca4d052c1924c5
Saad Ahmad <saadkwi12@hotmail.com>
2024-03-08 12:52:04 +1100

    update

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -6,8 +6,8 @@ locals {
   version            = 1
   name_prefix        = "${local.short_project_name}${local.stage}${local.version}"
   tags = {
-    project_name       = var.PROJECT_NAME
-    short_project_name = var.SHORT_PROJECT_NAME
+    project_name       = var.project_name
+    short_project_name = var.short_project_name
     version            = local.version
     infrastructure     = "core"
     stage              = local.stage
